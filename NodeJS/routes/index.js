ce9fb845eaf5215ff1c0269e2763c10472707f71
igormzg <igormzg@gmail.com>
2015-11-25 23:02:46 +0200

    My first commit

---
+++ NodeJS/routes/index.js
@@ -0,0 +1,12 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express' });
+};
+
+exports.index2 = function (req, res) {
+    res.render('index2', { title: 'EJS Title', scripts: ['./javascripts/todo.js'] });
+};
